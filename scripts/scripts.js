71efd026dfe3a830355a98721550c52f00dab215
Ruslan Kushukov <ruslanoid91@gmail.com>
2022-04-14 19:20:28 +0300

    2nd check

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,16 +1,20 @@
 const popup = document.querySelector('.popup');
 const openPopupButton = document.querySelector('.profile__edit-button');
 const closePopupButton = popup.querySelector('.popup__close-button');
+const formElement = document.querySelector('.popup__placeholder');
+let nameInput = document.querySelector('.popup__placeholder-input_type_name');
+const passionInput = document.querySelector('.popup__placeholder-input_type_passion');
+let nameOrigin = document.querySelector('.profile__info-name');
+const passionOrigin = document.querySelector('.profile__info-passion');
 
 function popupEnableToggle() {
-    popup.classList.add('popup__enable');
+    popup.classList.add('popup_enable');
+    nameInput.value = nameOrigin.textContent
+    passionInput.value = passionOrigin.textContent;
 }
 
-//Если пользователь ввел данные и не сохранил их.
 function popupDisableToggle() {
-    popup.classList.remove('popup__enable');
-    nameInput.value = nameOrigin.textContent
-    passionInput.value = passionInput.value;
+    popup.classList.remove('popup_enable');
 }
 
 function popupOverlayClickHandler(evt) {
@@ -19,24 +23,15 @@ function popupOverlayClickHandler(evt) {
     }
 }
 
-
+function submitToogle(event) {
+    event.preventDefault();
+    nameOrigin.textContent = nameInput.value;
+    passionOrigin.textContent = passionInput.value;
+    popupDisableToggle();
+}
 
 openPopupButton.addEventListener('click', popupEnableToggle);
 closePopupButton.addEventListener('click', popupDisableToggle);
 popup.addEventListener('click', popupOverlayClickHandler);
-
-const formElement = document.querySelector('.popup__placeholder');
-let nameInput = document.querySelector('.popup__placeholder-name');
-const passionInput = document.querySelector('.popup__placeholder-passion');
-let nameOrigin = document.querySelector('.profile__info-name');
-const passionOrigin = document.querySelector('.profile__info-passion');
-
-document.querySelector('.popup__placeholder').addEventListener('submit', (event) => {
-    event.preventDefault();
-
-    nameOrigin.textContent = nameInput.value;
-    passionOrigin.textContent = passionInput.value;
-
-    popupDisableToggle();
-
-})
+formElement.addEventListener('submit', submitToogle);
+  
